d8bb2cf9720f12a0e17d20867349d375b78b91b7
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-05-03 23:12:40 +0900

    Update reports_backTesting.py
    증권사 리포트의 주가 목표치 상향 후 주가는 어떻게 되었는가?

--- reports_backTesting.py
+++ reports_backTesting.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from pykrx import stock
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from mpl_finance import candlestick_ohlc
 
@@ -17,9 +17,32 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 
+from selenium import webdriver
+
+import time
+
 
 font_path = "./font/godoFont_all/godoMaum.ttf"
 font_prop = plt_fm.FontProperties(fname=font_path, size=18)
+
+
+def get_unit_bid_price(code):
+    opt = webdriver.ChromeOptions()
+    opt.add_argument('headless')
+
+    drv = webdriver.Chrome('./chromedriver.exe', options=opt)
+    drv.implicitly_wait(3)
+    base_url = 'https://finance.naver.com/item/sise.nhn?code='
+    code = str(code)
+    drv.get(base_url+code)
+
+    bs = BeautifulSoup(drv.page_source, 'lxml')
+    drv.quit()
+
+    temp = bs.find_all('table', class_='type2')[1]
+    sub_temp = temp.find_all('span', class_='tah p11 nv01')
+
+    return int(sub_temp[1].get_text().strip().replace(',', '')) - int(sub_temp[3].get_text().strip().replace(',', ''))
 
 
 def get_reports():
@@ -71,13 +94,11 @@ def get_reports():
                         """
                         포함 조건
                         - 유효한 증권 회사의 리포트
-                        effective_companys = ['하나금융투자', '이베스트증권', 'IBK투자증권', '미래에셋증권', '키움증권']
+                        company_list = ['하나금융투자', '이베스트증권', 'IBK투자증권', '미래에셋증권', '키움증권']
                         - 목표가 제시                    
                         """
 
-                        effective_companys = ['하나금융투자', '이베스트증권', 'IBK투자증권', '미래에셋증권', '키움증권' ]
-
-                        if company not in effective_companys:
+                        if company not in company_list:
                             continue
                         if price == '없음':
                             continue
@@ -102,38 +123,85 @@ def get_reports():
         writer.close()
 
 
-def read_reports():
-    df = pd.read_excel('reports.xlsx', engine='openpyxl', index_col=0)
-    df = df.sort_values(by='date', ascending=True)
-    df['code'] = df['code'].map('{:06d}'.format)
+"""
+매개변수
+1) key = 'code'
+stocks['code']: [list1, list2, list3, ...]
+list1: 증권사1의 report list of (date, tp, opi)
+2) key = '증권사'
+stocks = [dict1, dict2, dict3, ...]
+dict1: 증권사1의 dict
+dict['code']: [(date1, tp1, opi1), date2, tp2, opi2), ...]
+"""
+def read_reports(fromDate, toDate, key='code'):
+    global num_company
+    if key == 'code':
+        df = pd.read_excel('reports.xlsx', engine='openpyxl', index_col=0)
+        df = df.sort_values(by='date', ascending=True)
+        df['code'] = df['code'].map('{:06d}'.format)
 
-    stocks = dict()
-    num_company = 5
-    company_list = ['하나금융투자', '이베스트증권', 'IBK투자증권', '미래에셋증권', '키움증권']
+        stocks = dict()
 
-    for i in range(len(df)):
-        data = df.iloc[i]
-        code = data['code']
-        date = data['date']
-        tp = data['target price']
-        opi = data['opinion']
+        for i in range(len(df)):
+            data = df.iloc[i]
+            code = data['code']
+            date = data['date']
+            tp = data['target price']
+            opi = data['opinion']
 
-        if not stocks.get(code):
-            stocks[code] = [[] for _ in range(num_company)]
+            # fromDate ~ toDate 외의 데이터 제외
+            fromDate = datetime.strptime(fromDate, "%Y%m%d").date()
+            toDate = datetime.strptime(toDate, "%Y%m%d").date()
+            date_d = datetime.strptime(date, "%y.%m.%d").date()
+            if not fromDate < date_d < toDate:
+                continue
 
-        for j in range(num_company):
-            if company_list[j] == data['company']:
-                break
-        stocks[code][j].append((date, tp, opi))
+            if not stocks.get(code):
+                stocks[code] = [[] for _ in range(num_company)]
+
+            for j in range(num_company):
+                if company_list[j] == data['company']:
+                    break
+            stocks[code][j].append((date, tp, opi))
+
+    elif key == '증권사':
+        df = pd.read_excel('reports.xlsx', engine='openpyxl', index_col=0)
+        df = df.sort_values(by='date', ascending=True)
+        df['code'] = df['code'].map('{:06d}'.format)
+
+        stocks = []
+        for i in range(num_company):
+            stocks.append(dict())
+
+        for i in range(len(df)):
+            data = df.iloc[i]
+            code = data['code']
+            date = data['date']
+            tp = data['target price']
+            opi = data['opinion']
+
+            # fromDate ~ toDate 외의 데이터 제외
+            fromDate_d = datetime.strptime(fromDate, "%Y%m%d").date()
+            toDate_d = datetime.strptime(toDate, "%Y%m%d").date()
+            date_d = datetime.strptime(date, "%y.%m.%d").date()
+            if not fromDate_d < date_d < toDate_d:
+                continue
+
+            for j in range(num_company):
+                if company_list[j] == data['company']:
+                    break
+            if not stocks[j].get(code):
+                stocks[j][code] = []
+            stocks[j][code].append((date, tp, opi))
 
     return stocks
 
 
-def plot_graph():
+def plot_graph(fromDate, toDate):
     if not os.path.isdir('./graphs'):
         os.mkdir('./graphs')
 
-    stocks = read_reports()
+    stocks = read_reports(fromDate=fromDate, toDate=toDate, key='code')
     for key, value in stocks.items():
         code = key
         """
@@ -144,10 +212,11 @@ def plot_graph():
         value[4]: 키움증권
         """
         # 주가
-        df = stock.get_market_ohlcv_by_date("20180101", "20210423", code)
+        time.sleep(1)
+        df = stock.get_market_ohlcv_by_date(fromDate, toDate, code)
         df['number'] = df.index.map(mdates.date2num)
         # 코스피
-        kospi = stock.get_index_ohlcv_by_date("20180101", "20210423", "1001")
+        kospi = stock.get_index_ohlcv_by_date(fromDate, toDate, "1001")
         kospi['number'] = kospi.index.map(mdates.date2num)
         # Empty df인 경우
         if df.empty:
@@ -167,7 +236,7 @@ def plot_graph():
         # plt.grid(True)
         candlestick_ohlc(p1, ohlc.values, width=.6, colorup='red', colordown='blue')
         p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        for i in range(1):
+        for i in range(1, 2):
             for j in range(1, len(value[i])):
                 val = value[i][j]
                 day = val[0]
@@ -193,6 +262,147 @@ def plot_graph():
         plt.close()
 
 
-#get_reports()
-plot_graph()
+"""
+매개 변수
+upper: upper % 이상 -> 익절
+lower: lower % 이하 -> 손절
+duration: duration 일 이후 -> 매도
+"""
+def calculate_yields(fromDate, toDate, upper=20, lower=20, duration=181):
+    stocks = read_reports(fromDate=fromDate, toDate=toDate, key='증권사')
+    writer = pd.ExcelWriter('./yields.xlsx', engine='xlsxwriter')
+    # i: 각 증권사
+    for i in range(num_company):
+        code_list = []
+        yield_list = []
+        for key, value in stocks[i].items():
+            code = key
+            # 주가
+            today = datetime.strftime(datetime.now().date() - timedelta(days=10), "%Y%m%d")
+            df = stock.get_market_ohlcv_by_date(fromDate, today, code)
+            # 코스피
+            #kospi = stock.get_index_ohlcv_by_date(fromDate, today, "1001")
+            # Empty df인 경우
+            if df.empty:
+                continue
+            # df의 start date 및 end date check
+            if df.index[0] - datetime.strptime(fromDate, "%Y%m%d") > timedelta(days=10):
+                continue
+            if df.index[-1] < datetime.strptime(today, "%Y%m%d") - timedelta(days=10):
+                continue
+            name = stock.get_market_ticker_name(code)
+            # 거래 정지 기간 처리
+            idx_list = df.index[df.시가 == 0]
+            for idx in idx_list:
+                close = df.loc[idx].종가
+                df.at[idx, '시가'] = close
+                df.at[idx, '고가'] = close
+                df.at[idx, '저가'] = close
+    
+            index_list = df.index
+            """
+            매수 알고리즘: 증권사 목표 주가 상향
+            --> 익일 시가 매수
+            [(날짜1, 가격1), (날짜2, 가격2), ]
+            날짜: datetime 객체 %Y-%m-%d
+            """
+            buy_report = []
+            # val: 개별 리포트
+            for idx in range(1, len(value)):
+                val = value[idx]
+                day = val[0]
+                tp = val[1]
+                tp_past = value[idx - 1][1]
+                """
+                증권사 목표 주가 상향한날짜 이후의 유효한 날짜 검색
+                """
+                while (True):
+                    day = datetime.strptime(day, "%y.%m.%d").date() + timedelta(days=1)
+                    if day in index_list:
+                        break
+                    day = datetime.strftime(day, "%y.%m.%d")
+                day_s = datetime.strftime(day, "%Y-%m-%d")
+                if tp > tp_past:
+                    buy_report.append((day, df.loc[day_s]['시가']))
+            """
+            매도 알고리즘
+            [(날짜1, 가격1), (날짜2, 가격2), ]
+            날짜: datetime 객체 %Y-%m-%d
+            """
+            sell_report = []
+            for buy in buy_report:
+                day = buy[0]
+                price = buy[1]
+                upper_limit = price * (1 + upper/100)
+                lower_limit = price * (1 - lower/100)
+                # unit_bid = get_unit_bid_price(code)
+                # if upper_limit % unit_bid == 0:
+                #     upper_price = upper_limit
+                # else:
+                #     upper_price = (upper_limit + unit_bid) // unit_bid * unit_bid
+                # if lower_limit % unit_bid == 0:
+                #     lower_price = lower_limit
+                # else:
+                #     lower_price = (lower_limit - unit_bid) // unit_bid * unit_bid
+                # 6개월 내에 + upper 혹은 - lower 매도 조건 check
+                sell_flag = False
+                for count in range(duration):
+                    # 개장일이 아니라면 pass
+                    if not day in index_list:
+                        day = day + timedelta(days=1)
+                        continue
+                    # 개장일이라면 매도 조건 확인
+                    day_s = datetime.strftime(day, "%Y-%m-%d")
+                    # 익절 조건 확인
+                    if df.loc[day_s]['저가'] < upper_limit < df.loc[day_s]['고가']:
+                        sell_report.append((day, upper_limit))
+                        sell_flag = True
+                        break
+                    # 손절 조건 확인
+                    if df.loc[day_s]['저가'] < lower_limit < df.loc[day_s]['고가']:
+                        sell_report.append((day, lower_limit))
+                        sell_flag = True
+                        break
+                    # 익절/손절 조건 발생하지 않은 경우
+                    day = day + timedelta(days=1)
+                # duration 동안 익절/손절 조건 발생하지 않은 경우
+                # 개장일 체크
+                while (True):
+                    if day in index_list:
+                        break
+                    day = day + timedelta(days=1)
+                day_s = datetime.strftime(day, "%Y-%m-%d")
+                if not sell_flag:
+                    sell_report.append((day, df.loc[day_s]['종가']))
 
+            print(code)
+            print(buy_report)
+            print(sell_report)
+            # 수익률 계산
+            strat_yield_with_fee = 1
+            for idx in range(len(buy_report)):
+                delta = (0.99685 * sell_report[idx][1] - 1.00015 * buy_report[idx][1]) / buy_report[idx][1]
+                strat_yield_with_fee = strat_yield_with_fee * (1 + delta)
+            print(strat_yield_with_fee)
+            code_list.append(code)
+            yield_list.append(strat_yield_with_fee)
+
+        data = {'code': code_list, 'yields': yield_list}
+        dd = pd.DataFrame(data=data)
+        #writer = pd.ExcelWriter('./yields.xlsx', engine="openpyxl", mode='a')
+        dd.to_excel(writer, sheet_name=company_list[i])
+    writer.close()
+
+
+"""
+증권사 리스트 지정
+"""
+num_company = 5
+company_list = ['하나금융투자', '이베스트증권', 'IBK투자증권', '미래에셋증권', '키움증권']
+
+fromDate = '20170101'
+toDate = '20200930'
+
+#get_reports()
+#plot_graph(fromDate=fromDate, toDate=toDate)
+calculate_yields(fromDate=fromDate, toDate=toDate)
