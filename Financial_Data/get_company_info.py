ceb1d5e638988f432f2d4dd09596b201448038c5
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2021-06-09 22:05:35 +0900

    Create get_company_info.py

---
+++ Financial_Data/get_company_info.py
@@ -0,0 +1,42 @@
+
+"""
+get_company_info.py
+Dart 공시 주식회사 Info Excel File 생성
+"""
+import dart_fss as dart
+import pandas as pd
+
+"""
+API Key
+"""
+api_key = '423358f7b1762af4cf81bb633c44244636f9037c'
+dart.set_api_key(api_key=api_key)
+
+"""
+공시 기업 정리
+"""
+data = dart.api.filings.get_corp_code()
+
+corp_code_list = []
+corp_name_list = []
+stock_code_list= []
+modify_date_list = []
+
+dart_company_info_list = [corp_code_list, corp_name_list, stock_code_list, modify_date_list]
+
+for i in range(len(data)):
+    temp_list = []
+    for key, val in data[i].items():
+        temp_list.append(val)
+    if temp_list[2] is None:
+        continue
+    for j, temp in enumerate(temp_list):
+        dart_company_info_list[j].append(temp)
+
+data = {'Code': corp_code_list, 'Name': corp_name_list, 'Stock Code': stock_code_list, 'Last Date': modify_date_list}
+df = pd.DataFrame(data=data)
+writer = pd.ExcelWriter('./company_info.xlsx', engine='xlsxwriter')
+df.to_excel(writer)
+writer.close()
+
+
